5d92c55e804e4d30f157ccff293f924dc1546024
hsloss <hannahmaguiresloss@gmail.com>
2018-12-11 13:08:32 -0500

    added weather for dynamic lat and lng

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # server-project
+
+feature 1: location-controller
+
+given address query of city, return latitude and longitude from Google Maps Geocoding API.
+
+feature 2: weather-controller
+
+given address query of city, return current time and current weather conditions from Dark Sky API.
--- server.js
+++ server.js
@@ -7,30 +7,52 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
-app.get('/location',(req, res)=>
-{
+
+app.get('/location', locationController)
+
+app.get('/weather', weatherController)
+
+app.get('/', (req, res) => {
+  res.send('<div>This is the Home Route</div>')
+})
+
+app.use('*', (req, res) => {
+  res.send('<img src="https://http.cat/404" />')
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
+
+function locationController(req, res) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.address}&key=${process.env.GOOGLE_API_KEY}`
   superagent.get(url)
     .then(result=>{
-      console.log(result)
       res.send(new Location(result))
     })
-    .catch(err=>res.send(err))})
+    .catch(err=>res.send(err))
+}
 
-app.get('/', (req,res)=>{
-  res.send('<h1>This is the Correct route</h1>')
-})
-
-app.get('*', (req, res) =>
-  res.send('500 error message'))
-
-
-
-app.listen(PORT, ()=>{
-  console.log(`Listening on port ${PORT}`)
-})
+function weatherController(req, res) {
+  // const url = 'https://api.darksky.net/forecast/d3f4d353a9097935306705f81711b6da/37.8267,-122.4233'
+  const url =`https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${req.query.lat},${req.query.lng}`
+  superagent.get(url)
+    .then(result => {
+      const newWeather = new WeatherConstructor(result)
+      console.log(result)
+      res.send(newWeather)
+    })
+    .catch(err=>res.send(err))
+}
 
 const Location = function(loc){
   this.lat = loc.body.results[0].geometry.location.lat
   this.lng = loc.body.results[0].geometry.location.lng
+}
+
+const WeatherConstructor = function(weather) {
+  this.time = weather.body.currently.time
+  this.summary = weather.body.currently.summary
+  this.temp = weather.body.currently.temperature
 }
